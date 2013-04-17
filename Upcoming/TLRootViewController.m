424826c903e96a50e05409a8b0546e39e14424d8
Ash Furrow <ash@ashfurrow.com>
2013-04-17 14:25:02 -0400

    Basics gestures down.

--- Upcoming/TLRootViewController.m
+++ Upcoming/TLRootViewController.m
@@ -18,9 +18,15 @@ static const CGFloat headerHeight = 72.0f;
 @property (nonatomic, strong) TLDayListViewController *dayListViewController;
 @property (nonatomic, strong) TLHeaderViewController *headerViewController;
 
+@property (nonatomic, strong) RACSubject *panSubject;
+@property (nonatomic, strong) RACSubject *dayListMovementSubject;
+
 @end
 
 @implementation TLRootViewController
+
+static const CGFloat kMaximumTranslationThreshold = 88.0f;
+static const CGFloat kMaximumShrinkTranslation = 0.15f;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -31,6 +37,34 @@ static const CGFloat headerHeight = 72.0f;
     
     self.headerViewController = [[TLHeaderViewController alloc] initWithNibName:@"TLHeaderViewController" bundle:nil];
     [self addChildViewController:self.headerViewController];
+    
+    self.dayListMovementSubject = [RACSubject subject];
+    [self.dayListMovementSubject subscribeNext:^(id x) {
+        CGFloat ratio = [x floatValue];
+        
+        if (ratio < 0.01)
+        {
+            self.dayListViewController.view.transform = CGAffineTransformIdentity;
+        }
+        else
+        {
+            self.dayListViewController.view.transform = CGAffineTransformMakeScale(1.0f - ratio * kMaximumShrinkTranslation, 1.0f - ratio * kMaximumShrinkTranslation);
+        }
+    }];
+    
+    self.panSubject = [RACSubject subject];
+    [self.panSubject subscribeNext:^(NSNumber *translation) {
+        CGFloat verticalTranslation = [translation floatValue];
+        
+        if (verticalTranslation <= 0)
+        {
+            [self.dayListMovementSubject sendNext:@(0)];
+        }
+        else
+        {
+            [self.dayListMovementSubject sendNext:@(MIN(fabsf(verticalTranslation / kMaximumTranslationThreshold), 1))];
+        }
+    }];
     
     return self;
 }
@@ -46,6 +80,32 @@ static const CGFloat headerHeight = 72.0f;
     self.headerViewController.view.frame = CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), headerHeight);
     self.headerViewController.view.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin;
     [self.view addSubview:self.headerViewController.view];
+}
+
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor colorWithWhite:33.0f/255.0f alpha:1.0f];
+    
+    UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(userDidPan:)];
+    [self.headerViewController.view addGestureRecognizer:panGestureRecognizer];
+}
+
+-(void)userDidPan:(UIPanGestureRecognizer *)recognizer
+{
+    CGPoint translation = [recognizer translationInView:self.view];
+    
+    if (recognizer.state == UIGestureRecognizerStateChanged)
+    {
+        [self.panSubject sendNext:@(translation.y)];
+    }
+    else if (recognizer.state == UIGestureRecognizerStateEnded)
+    {
+        [UIView animateWithDuration:0.25f animations:^{
+            [self.panSubject sendNext:@(0)];
+        }];
+    }
 }
 
 @end
