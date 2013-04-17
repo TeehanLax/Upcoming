5241676e271482dae8149d6b3535b789b4874fb6
Ash Furrow <ash@ashfurrow.com>
2013-04-17 15:48:55 -0400

    Added tap gesture to return to default state.

--- Upcoming/TLRootViewController.m
+++ Upcoming/TLRootViewController.m
@@ -14,7 +14,7 @@
 #import <BlocksKit.h>
 #import <EXTScope.h>
 
-static const CGFloat headerHeight = 72.0f;
+static const CGFloat kHeaderHeight = 72.0f;
 
 @interface TLRootViewController ()
 
@@ -23,16 +23,19 @@ static const CGFloat headerHeight = 72.0f;
 
 @property (nonatomic, strong) UIView *dayListOverlayView;
 
+@property (nonatomic, strong) UITapGestureRecognizer *tapGestureRecognizer;
+
 @property (nonatomic, strong) RACSubject *downwardPanSubject;
 @property (nonatomic, strong) RACSubject *dayListMovementSubject;
+@property (nonatomic, strong) RACSubject *headerMovementSubject;
 @property (nonatomic, strong) RACSubject *dayListOverlaySubject;
 
 @end
 
 @implementation TLRootViewController
 
-static const CGFloat kMaximumTranslationThreshold = 88.0f;
-static const CGFloat kMaximumShrinkTranslation = 0.15f;
+static const CGFloat kMaximumTranslationThreshold = 320.0f;
+static const CGFloat kMaximumShrinkTranslation = 0.1f;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -45,7 +48,7 @@ static const CGFloat kMaximumShrinkTranslation = 0.15f;
     [self addChildViewController:self.headerViewController];
     
     self.dayListOverlayView = [[UIView alloc] initWithFrame:self.dayListViewController.view.frame];
-    self.dayListOverlayView.backgroundColor = [UIColor colorWithWhite:0.0f alpha:0.2f];
+    self.dayListOverlayView.backgroundColor = [UIColor colorWithWhite:0.0f alpha:0.4f];
     self.dayListOverlayView.alpha = 0.0f;
     
     @weakify(self);
@@ -63,6 +66,22 @@ static const CGFloat kMaximumShrinkTranslation = 0.15f;
             self.dayListViewController.view.userInteractionEnabled = YES;
             [self.dayListOverlayView removeFromSuperview];
         }
+    }];
+    
+    self.headerMovementSubject = [RACSubject subject];
+    [self.headerMovementSubject subscribeNext:^(id x) {
+        @strongify(self);
+        
+        CGFloat translation = [x floatValue];
+        
+        CGRect frame = CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), kHeaderHeight + translation);
+        
+        if (translation < 0)
+        {
+            frame = CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), kHeaderHeight);
+        }
+        
+        self.headerViewController.view.frame = frame;
     }];
     
     self.dayListMovementSubject = [RACSubject subject];
@@ -115,7 +134,7 @@ static const CGFloat kMaximumShrinkTranslation = 0.15f;
 {
     [super loadView];
     
-    self.dayListViewController.view.frame = CGRectMake(0, headerHeight, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds) - headerHeight);
+    self.dayListViewController.view.frame = CGRectMake(0, kHeaderHeight, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds) - kHeaderHeight);
     self.dayListViewController.view.autoresizingMask = UIViewAutoresizingFlexibleHeight;
     self.dayListViewController.view.layer.shadowColor = [[UIColor blackColor] CGColor];
     self.dayListViewController.view.layer.shadowOpacity = 1.0f;
@@ -126,8 +145,7 @@ static const CGFloat kMaximumShrinkTranslation = 0.15f;
     
     [self.view addSubview:self.dayListViewController.view];
     
-    self.headerViewController.view.frame = CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), headerHeight);
-    self.headerViewController.view.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin;
+    self.headerViewController.view.frame = CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), kHeaderHeight);
     [self.view addSubview:self.headerViewController.view];
 }
 
@@ -137,7 +155,27 @@ static const CGFloat kMaximumShrinkTranslation = 0.15f;
     
     self.view.backgroundColor = [UIColor colorWithWhite:33.0f/255.0f alpha:1.0f];
     
-    UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithHandler:^(UIGestureRecognizer *sender, UIGestureRecognizerState state, CGPoint location) {
+    self.tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithHandler:^(UIGestureRecognizer *sender, UIGestureRecognizerState state, CGPoint location) {
+        if (CGRectContainsPoint(self.headerViewController.view.frame, location)) return;
+        
+        [UIView animateWithDuration:0.25f animations:^{
+            [self.downwardPanSubject sendNext:@(0)];
+            [self.headerMovementSubject sendNext:@(0)];
+        } completion:^(BOOL finished) {
+            [self.dayListOverlaySubject sendNext:@(NO)];
+        }];
+    }];
+    
+    [self.view addGestureRecognizer:self.tapGestureRecognizer];
+    
+    RAC(self.tapGestureRecognizer.enabled) = [RACSignal combineLatest:@[RACAble(self.headerViewController.view.frame)]
+                                                               reduce:^(NSValue *frameValue){
+                                                                   CGRect frame = [frameValue CGRectValue];
+                                                                   
+                                                                   return @(CGRectGetHeight(frame) >= kMaximumTranslationThreshold);
+                                                               }];
+    
+    UIPanGestureRecognizer *panDownGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithHandler:^(UIGestureRecognizer *sender, UIGestureRecognizerState state, CGPoint location) {
         UIPanGestureRecognizer *recognizer = (UIPanGestureRecognizer *)sender;
         
         CGPoint translation = [recognizer translationInView:self.view];
@@ -149,6 +187,7 @@ static const CGFloat kMaximumShrinkTranslation = 0.15f;
         else if (state == UIGestureRecognizerStateChanged)
         {
             [self.downwardPanSubject sendNext:@(translation.y)];
+            [self.headerMovementSubject sendNext:@(translation.y)];
         }
         else if (state == UIGestureRecognizerStateEnded)
         {
@@ -158,10 +197,12 @@ static const CGFloat kMaximumShrinkTranslation = 0.15f;
                 if (movingDown)
                 {
                     [self.downwardPanSubject sendNext:@(kMaximumTranslationThreshold)];
+                    [self.headerMovementSubject sendNext:@(kMaximumTranslationThreshold)];
                 }
                 else
                 {
                     [self.downwardPanSubject sendNext:@(0)];
+                    [self.headerMovementSubject sendNext:@(0)];
                 }
             } completion:^(BOOL finished) {
                 if (!movingDown)
@@ -171,7 +212,8 @@ static const CGFloat kMaximumShrinkTranslation = 0.15f;
             }];
         }
     }];
-    [self.headerViewController.view addGestureRecognizer:panGestureRecognizer];
+    
+    [self.headerViewController.view addGestureRecognizer:panDownGestureRecognizer];
 }
 
 
