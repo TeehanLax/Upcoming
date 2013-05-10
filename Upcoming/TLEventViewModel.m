6b35a242ce287c3e07f96a0bbdf498f849e1dc74
Ash Furrow <ash@ashfurrow.com>
2013-05-10 13:45:53 -0400

    Added rounding for short events.

--- Upcoming/TLEventViewModel.m
+++ Upcoming/TLEventViewModel.m
@@ -7,6 +7,14 @@
 //
 
 #import "TLEventViewModel.h"
+#import "EKEventManager.h"
+
+@interface NSDate (Rounding)
+
+-(NSDate *)dateByRoundingUpToNearestHalfHour;
+-(NSDate *)dateByRoundingDownToNearestHalfHour;
+
+@end
 
 @implementation TLEventViewModel
 
@@ -22,13 +30,52 @@
      (ie: events "touch" but don't overlap)
      
      */
+    
+    NSDate *ourStartDate = [self.event.startDate dateByRoundingDownToNearestHalfHour];
+    NSDate *ourEndDate = [self.event.endDate dateByRoundingUpToNearestHalfHour];
+    NSDate *theirStartDate = [otherModel.event.startDate dateByRoundingDownToNearestHalfHour];
+    NSDate *theirEndDate = [otherModel.event.endDate dateByRoundingUpToNearestHalfHour];
+    
     BOOL overlaps =
-        ([self.event.endDate isLaterThanDate:otherModel.event.startDate] && [self.event.startDate isEarlierThanDate:otherModel.event.startDate] && ![self.event.endDate isEqualToDate:otherModel.event.startDate]) ||
-        ([self.event.startDate isEarlierThanDate:otherModel.event.endDate] && [self.event.endDate isLaterThanDate:otherModel.event.endDate] && ![self.event.startDate isEqualToDate:otherModel.event.endDate]) ||
-        ([self.event.startDate isLaterThanDate:otherModel.event.startDate] && [self.event.endDate isEarlierThanDate:otherModel.event.endDate]) ||
-        ([self.event.startDate isEarlierThanDate:otherModel.event.startDate] && [self.event.endDate isLaterThanDate:otherModel.event.endDate]);
+        ([ourEndDate isLaterThanDate:theirStartDate] && [ourStartDate isEarlierThanDate:theirStartDate] && ![ourEndDate isEqualToDate:theirStartDate]) ||
+        ([ourStartDate isEarlierThanDate:theirEndDate] && [ourEndDate isLaterThanDate:theirEndDate] && ![ourStartDate isEqualToDate:theirEndDate]) ||
+        ([ourStartDate isLaterThanDate:theirStartDate] && [ourEndDate isEarlierThanDate:theirEndDate]) ||
+        ([ourStartDate isEarlierThanDate:theirStartDate] && [ourEndDate isLaterThanDate:theirEndDate]);
 
     return overlaps;
+}
+
+@end
+
+
+@implementation NSDate (Rounding)
+
+-(NSDate *)dateByRoundingUpToNearestHalfHour {
+    NSDateComponents *components = [[[EKEventManager sharedInstance] calendar] components:(NSDayCalendarUnit | NSHourCalendarUnit | NSMinuteCalendarUnit) fromDate:self];
+    
+    NSInteger minutes = components.minute;
+    
+    if (minutes < 30) { minutes = 0; }
+    else { minutes = 30; }
+    
+    components.minute = minutes;
+    
+    return [[NSCalendar currentCalendar] dateFromComponents:components];
+}
+
+-(NSDate *)dateByRoundingDownToNearestHalfHour {
+    NSDateComponents *components = [[[EKEventManager sharedInstance] calendar] components:(NSDayCalendarUnit | NSHourCalendarUnit | NSMinuteCalendarUnit) fromDate:self];
+    
+    NSInteger minutes = components.minute;
+    NSInteger hours = components.hour;
+    
+    if (minutes <= 30) { minutes = 30; }
+    else { minutes = 0; hours++; }
+    
+    components.minute = minutes;
+    components.hour = hours;
+    
+    return [[NSCalendar currentCalendar] dateFromComponents:components];
 }
 
 @end
